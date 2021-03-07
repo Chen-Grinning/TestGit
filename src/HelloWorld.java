321bc6bc249bd9faed8fc87db6e404d5042631ff
doge <975134457@qq.com>
2021-03-07 16:04:52 +0800

    The second change!Test change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
+    //   Project configurations files can be added to Git
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
 }
-//   Project configurations files can be added to Git
+
---
+++ src/LiangZhiWei.java
@@ -0,0 +1,5 @@
+public class LiangZhiWei {
+    public static void main(String[] args) {
+        System.out.println("我是梁志伟");
+    }
+}
